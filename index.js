5ec6127729563aeffb747c1241f81117b002f382
Andrew Harris <andrew@noblesamurai.com>
2017-06-21 13:59:58 +1000

    implement crop

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ class Cover {
    * @param {number} y offset y coordinate on the canvas
    * @param {number} width width to fit to on the canvas
    * @param {number} height height to fit to on the canvas
+   * @returns {Cover}
    */
   constructor (img, x, y, width, height) {
     const ir = img.width / img.height;
@@ -17,37 +18,53 @@ class Cover {
     this.y = y;
     this.width = width;
     this.height = height;
-    this.bounds = [];
+    this.bounds = [{ width: img.width, height: img.height, sx: 0, sy: 0 }];
     this.sw = ir < r ? img.width : img.height * r;
     this.sh = ir < r ? img.width / r : img.height;
     this.pan(0.5, 0.5);
   }
 
   /**
+   * Doesn't actually crop the input image but does redefine the bounds of the
+   * image for the sake of panning. ie. after a crop, the pan cx and cy will
+   * be with regard to the currently defined area rather than the whole image
+   * or previously cropped area.
+   *
+   * @returns {Cover}
+   */
+  crop () {
+    let { sw: width, sh: height, sx, sy } = this;
+    this.bounds.push({ width, height, sx, sy });
+    return this;
+  }
+
+  /**
    * Change the center point of the image.
+   *
    * @param {number} cx value between 0 and 1 representing the left or right
-   *   side of the image bounds. The image bounds will be the image area prior
-   *   to the last zoom operation. ie. initially the pan area is the whole
-   *   image. Then after each zoom it will be the area prior to that zoom.
+   *   side of the image bounds. The bounds will be the whole image or the
+   *   defined source area at the time of the last crop().
    * @param {number} cy value between 0 and 1 representing the top or the
    *   bottom of the image bounds.
+   * @returns {Cover}
    */
   pan (cx, cy) {
     if (cx < 0 || cx > 1) throw new Error('make sure 0 < cx < 1 ');
     if (cy < 0 || cy > 1) throw new Error('make sure 0 < cy < 1 ');
-    let { width, height, sx, sy } = this.bounds.length > 0 ? this.bounds[this.bounds.length - 1] : this.img;
-    this.sx = (sx || 0) + (width - this.sw) * cx;
-    this.sy = (sy || 0) + (height - this.sh) * cy;
+    let { width, height, sx, sy } = this.bounds[this.bounds.length - 1];
+    this.sx = sx + (width - this.sw) * cx;
+    this.sy = sy + (height - this.sh) * cy;
     return this;
   }
 
   /**
    * Zoom in at the current location.
+   *
    * @param {number} factor how much to zoom in by (>=1).
+   * @returns {Cover}
    */
   zoom (factor) {
     if (factor < 1) throw new Error('zoom not >= 1');
-    this.bounds.push({ width: this.sw, height: this.sh, sx: (this.sx || 0), sy: (this.sy || 0) });
     this.sx += (this.sw - (this.sw / factor)) / 2;
     this.sy += (this.sh - (this.sh / factor)) / 2;
     this.sw /= factor;
@@ -57,7 +74,9 @@ class Cover {
 
   /**
    * Render to the provided context.
+   *
    * @param {CanvasRenderingContext2D} ctx canvas context to render to
+   * @returns {Cover}
    */
   render (ctx) {
     ctx.drawImage(this.img, this.sx, this.sy, this.sw, this.sh, // source
--- test/index.js
+++ test/index.js
@@ -15,42 +15,6 @@ describe('canvas cover', function () {
     expect(out).to.include({ sx: 100, sy: 25, sw: 100, sh: 50 });
   });
 
-  it('should pan to the top left', () => {
-    const img = { width: 300, height: 100 };
-    const out = cover(img, 0, 0, 200, 100).pan(0, 0);
-    expect(out).to.include({ sx: 0, sy: 0, sw: 200, sh: 100 });
-  });
-
-  it('should pan to the bottom right of the pre-zoom area', () => {
-    const img = { width: 300, height: 100 };
-    const out = cover(img, 0, 0, 200, 100).zoom(2).pan(1, 1);
-    expect(out).to.include({ sx: 150, sy: 50, sw: 100, sh: 50 });
-  });
-
-  it('should zoom into the center of the left side of the image', () => {
-    const img = { width: 300, height: 100 };
-    const out = cover(img, 0, 0, 200, 100).pan(0, 0).zoom(2);
-    expect(out).to.include({ sx: 50, sy: 25, sw: 100, sh: 50 });
-  });
-
-  it('should handle multiple zooms and pans', () => {
-    const img = { width: 300, height: 100 };
-    const out = cover(img, 0, 0, 200, 100).pan(1, 1).zoom(2).pan(1, 1).zoom(2);
-    expect(out).to.include({ sx: 225, sy: 62.5, sw: 50, sh: 25 });
-  });
-
-  it('should zoom into the center of the left side of a wide image', () => {
-    const img = { width: 400, height: 100 };
-    const out = cover(img, 0, 0, 200, 100).pan(0, 0.5).zoom(2);
-    expect(out).to.include({ sx: 50, sy: 25, sw: 100, sh: 50 });
-  });
-
-  it('should be able to replicate css background-position for images', () => {
-    const img = { width: 300, height: 100 };
-    const out = cover(img, 0, 0, 200, 100).pan(0.25, 0.5).zoom(2).pan(0.25, 0.5);
-    expect(out).to.include({ sx: 50, sy: 25, sw: 100, sh: 50 });
-  });
-
   it('should render to a canvas context', () => {
     const ctx = {
       save: () => {},
@@ -58,8 +22,74 @@ describe('canvas cover', function () {
       restore: () => {}
     };
     const img = { width: 300, height: 100 };
-    cover(img, 0, 0, 200, 100).zoom(2).pan(0, 0).zoom(2).render(ctx);
+    cover(img, 0, 0, 200, 100).zoom(2).render(ctx);
     expect(ctx.drawImage.callCount).to.equal(1);
-    expect(ctx.drawImage.calledWith(img, 75, 12.5, 50, 25, 0, 0, 200, 100)).to.equal(true);
+    expect(ctx.drawImage.calledWith(img, 100, 25, 100, 50, 0, 0, 200, 100)).to.equal(true);
+  });
+
+  it('should shrink to fit if the render size is smaller than the image', () => {
+    const ctx = {
+      save: () => {},
+      drawImage: sinon.stub(),
+      restore: () => {}
+    };
+    const img = { width: 600, height: 200 };
+    cover(img, 0, 0, 200, 100).zoom(2).render(ctx);
+    expect(ctx.drawImage.callCount).to.equal(1);
+    expect(ctx.drawImage.calledWith(img, 200, 50, 200, 100, 0, 0, 200, 100)).to.equal(true);
+  });
+
+  it('should be able to render to a non 0 offset position in the canvas', () => {
+    const ctx = {
+      save: () => {},
+      drawImage: sinon.stub(),
+      restore: () => {}
+    };
+    const img = { width: 600, height: 200 };
+    cover(img, 50, 50, 200, 100).zoom(2).render(ctx);
+    expect(ctx.drawImage.callCount).to.equal(1);
+    expect(ctx.drawImage.calledWith(img, 200, 50, 200, 100, 50, 50, 200, 100)).to.equal(true);
+  });
+
+  it('should pan to the left', () => {
+    const img = { width: 300, height: 100 };
+    const out = cover(img, 0, 0, 200, 100).pan(0, 0.5);
+    expect(out).to.include({ sx: 0, sy: 0, sw: 200, sh: 100 });
+  });
+
+  it('should pan to the right', () => {
+    const img = { width: 300, height: 100 };
+    const out = cover(img, 0, 0, 200, 100).pan(1, 0.5);
+    expect(out).to.include({ sx: 100, sy: 0, sw: 200, sh: 100 });
+  });
+
+  it('should zoom into the top left', () => {
+    const img = { width: 300, height: 100 };
+    const out = cover(img, 0, 0, 200, 100).zoom(2).pan(0, 0);
+    expect(out).to.include({ sx: 0, sy: 0, sw: 100, sh: 50 });
+  });
+
+  it('should zoom into the bottom right', () => {
+    const img = { width: 300, height: 100 };
+    const out = cover(img, 0, 0, 200, 100).zoom(2).pan(1, 1);
+    expect(out).to.include({ sx: 200, sy: 50, sw: 100, sh: 50 });
+  });
+
+  it('should zoom into the center of the top left', () => {
+    const img = { width: 300, height: 100 };
+    const out = cover(img, 0, 0, 200, 100).zoom(2).pan(0, 0).zoom(2);
+    expect(out).to.include({ sx: 25, sy: 12.5, sw: 50, sh: 25 });
+  });
+
+  it('should zoom into the center of the bottom right', () => {
+    const img = { width: 300, height: 100 };
+    const out = cover(img, 0, 0, 200, 100).zoom(2).pan(1, 1).zoom(2);
+    expect(out).to.include({ sx: 225, sy: 62.5, sw: 50, sh: 25 });
+  });
+
+  it('should pan within a cropped area', () => {
+    const img = { width: 300, height: 100 };
+    const out = cover(img, 0, 0, 200, 100).crop().zoom(2).pan(1, 1);
+    expect(out).to.include({ sx: 150, sy: 50, sw: 100, sh: 50 });
   });
 });
